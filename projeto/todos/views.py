a6a7cbbc56fe17821a4cb8e61fecf41bf05b0e56
GAS097 <work.gabriel.as@gmail.com>
2025-03-13 00:28:10 -0300

    Criado o todo_list.html

--- projeto/todos/views.py
+++ projeto/todos/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from .models import Todo
 
 # Create your views here.
-def home (request):
-    return render (request, "todos/home.html")
+def todo_list_old(request):
+    nome = 'Gabriel'
+    alunos = ['1.Thiago', '2.Kayo', '3.Rodrigo']
+    return render(request, "todos/todo_list.html", {"nome": nome, "lista_alunos": alunos})
+
+def todo_list(request):
+    todos = Todo.objects.all()
+    return render(request, "todos/todo_list.html", {"todos": todos})
